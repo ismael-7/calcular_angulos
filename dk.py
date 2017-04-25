5ce601afc4f35c1050a989af8cb323f52740ebde
ismael-7 <ihidalgonv@alumnos.unex.es>
2017-04-25 10:03:59 +0200

    ik correcto

--- dk.py
+++ dk.py
@@ -1,9 +1,20 @@
 #! /usr/bin/python
 from math import *
-l1=1
-l2=1
-alpha=pi/4
-beta=-pi/2
-x=l1*cos(alpha)+l2*cos(alpha+beta)
-y=l1*sin(alpha)+l2*sin(alpha+beta)
-print (x,y)
+from ik import ik
+
+l1 = 1
+l2 = 1
+#alpha y beta tienen que ser float y beta < 0
+# para obtener lo mismo en el ik
+alpha = pi/4
+beta = -3*pi/4
+x = l1 * cos(alpha) + l2 * cos(alpha + beta)
+y = l1 * sin(alpha) + l2 * sin(alpha + beta)
+print "cinematica directa:"
+print "alpha, beta"
+print (alpha, beta)
+print "x,y"
+print (x, y)
+print "cinematica inversa:"
+print "alpha, beta"
+ik(x, y)
--- ik.py
+++ ik.py
@@ -1,40 +1,49 @@
 #! /usr/bin/python
 from math import *
-l1=1
-l2=1
-x=1.4142
-y=0
-r2=pow(x,2)+pow(y,2)
-aux=r2-pow(l1,2)-pow(l2,2)
-cosBeta=-aux/(float) (2*l1*l2)
-aux=1-pow(cosBeta, 2)
-senoBeta=sqrt(aux)
-beta=0.0
-if cosBeta!=0:
-    beta=atan(senoBeta/cosBeta)
-else:
-    beta=asin(senoBeta)
 
-if y<=pi/2:
- beta=pi+beta
-else:
-  beta = pi - beta
 
-aux= -pow(l2,2)+pow(l1,2)+r2
-cosAlpha=aux/(2*l1*sqrt(r2))
-aux=1-pow(cosAlpha, 2)
-senoAlpha=sqrt(aux)
-alpha=0.0
-if cosAlpha!=0:
-    alpha=atan(senoAlpha/cosAlpha)
-else:
-    alpha=asin(senoAlpha)
+def ik(x, y):
+    l1 = 1.0  # l1 y l2 tienen que ser float
+        l2 = 1.0
+        r2 = pow(x, 2) + pow(y, 2)
+        r = sqrt(r2)
+        numerador = -r2 + pow(l1, 2) + pow(l2, 2)
+        cosBetaP = numerador / (2 * l1 * l2)
+        radicando = 1 - pow(cosBetaP, 2)
+        senoBetaP = sqrt(radicando)
+        betaP = 0.0
+        if cosBetaP != 0:
+            betaP = atan(senoBetaP / cosBetaP)
+        else:
+            betaP = asin(senoBetaP)
 
-gama=0.0
-if r2!=0:
-    gama=asin(y/sqrt(r2))
-else:
-    gama=0
-alpha=gama+alpha
-beta=pi-beta
-print (alpha,beta)
+        senoAlphaP = l2 * senoBetaP / r
+        radicando = 1 - pow(senoAlphaP, 2)
+        cosAlphaP = sqrt(radicando)
+        alphaP = 0.0
+        if cosAlphaP != 0:
+            alphaP = atan(senoAlphaP / cosAlphaP)
+        else:
+            alpha = asin(senoAlphaP)
+
+        gama = 0.0
+        # if r2 != 0: TODO
+        #     gama = asin(y / sqrt(r2))
+        if x > 0:
+            gama = atan(y / x)
+        elif x == 0:
+            gama = asin(y / r)
+        elif y >= 0:
+            gama = (pi / 2) - atan(y / x)
+        else:
+            gama = -pi + atan(y / x)
+
+        alpha = gama + alphaP
+        beta = 0.0
+        # TODO esta parte es problematica: cuando betaP deberia ser 0 pero como hay cierta imprecision no lo es
+        if betaP <= 0:
+            beta = betaP
+        else:
+            beta = betaP - pi
+
+return alpha, beta
